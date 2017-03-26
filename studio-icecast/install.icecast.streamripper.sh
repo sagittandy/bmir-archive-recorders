ea1f0032ecaae93d711fa4d1962e495629d3bda5
sagittandy <sagittandy@gmail.com>
2017-03-26 17:33:33 -0400

    start icecast server

--- studio-icecast/install.icecast.streamripper.sh
+++ studio-icecast/install.icecast.streamripper.sh
@@ -12,10 +12,14 @@ export DELIMITER="-----------------------------------------------------"
 
 # Define custom values for Icecast config file.
 export ICECAST_CONFIG_FILE="/etc/icecast2/icecast.xml"
+export ICECAST_LOG_FILE_DIRECTORY="/var/log/icecast2"
+export ICECAST_PORT="8000"
 export ICECAST_HOSTNAME="localhost"
 export ICECAST_RELAY_PASSWORD="relaypw"
 export ICECAST_SOURCE_PASSWORD="sourcepw"
 export ICECAST_ADMIN_PASSWORD="adminpw"
+### export ICECAST_USER_NAME="icecastuser"
+### export ICECAST_USER_PASSWORD="userpw"
 
 
 echo ${DELIMITER}
@@ -35,6 +39,26 @@ if [ 0 != ${rc} ] ; then
 	echo "ERROR ${rc} apt-get dist-upgrade."
 	exit 1
 fi
+
+
+### echo ${DELIMITER}
+### echo "Create unix user ${ICECAST_USER_NAME}"
+### adduser ${ICECAST_USER_NAME}  --gecos "First Last,RoomNumber,WorkPhone,HomePhone" --disabled-password
+### rc=$?
+### if [ 0 != ${rc} ] ; then
+### 	echo "ERROR ${rc} creating unix user ${ICECAST_USER_NAME}"
+### 	exit 1
+### fi
+
+
+### echo ${DELIMITER}
+### echo "Set password for unix user ${ICECAST_USER_NAME}"
+### echo "${ICECAST_USER_NAME}:${ICECAST_USER_PASSWORD}" | chpasswd
+### rc=$?
+### if [ 0 != ${rc} ] ; then
+### 	echo "ERROR ${rc} setting password for unix user ${ICECAST_USER_NAME}"
+### 	exit 1
+### fi
 
 
 echo ${DELIMITER}
@@ -157,10 +181,84 @@ if [ 0 != ${rc} ] ; then
 fi
 
 
+echo ${DELIMITER}
+echo "Uncomment changeowner in icecast config file, step 1 of 2."
+sed -ie "s:<changeowner>: --> <changeowner>:g" ${ICECAST_CONFIG_FILE}
+rc=$?
+if [ 0 != ${rc} ] ; then
+	echo "ERROR ${rc} uncommenting changeowner in icecast config file, step 1 of 2."
+	exit 1
+fi
 
 
+echo ${DELIMITER}
+echo "Uncomment changeowner in icecast config file, step 2 of 2."
+sed -ie "s:</changeowner>:</changeowner> <!-- :g" ${ICECAST_CONFIG_FILE}
+rc=$?
+if [ 0 != ${rc} ] ; then
+    echo "ERROR ${rc} uncommenting changeowner in icecast config file, step 2 of 2."
+	exit 1
+fi
 
 
+echo ${DELIMITER}
+echo "Verify icecast2 log file directory exists ${ICECAST_LOG_FILE_DIRECTORY}"
+ls ${ICECAST_LOG_FILE_DIRECTORY}
+rc=$?
+if [ 0 != ${rc} ] ; then
+	echo "ERROR ${rc} Log file directory {ICECAST_LOG_FILE_DIRECTORY} does not exist."
+	exit 1
+fi
+
+
+echo ${DELIMITER}
+echo "Open permissions to write icecast log files. ${ICECAST_LOG_FILE_DIRECTORY}"
+chmod 777 ${ICECAST_LOG_FILE_DIRECTORY}
+rc=$?
+if [ 0 != ${rc} ] ; then
+    echo "ERROR ${rc} opening permissions to write icecast log files. ${ICECAST_LOG_FILE_DIRECTORY}"
+	exit 1
+fi
+
+
+echo ${DELIMITER}
+echo "Starting the server in 7 seconds... (else ctrl-c)"
+sleep 7
+
+
+echo ${DELIMITER}
+echo "Starting the icecat2 server."
+icecast2 -b -c ${ICECAST_CONFIG_FILE}
+rc=$?
+if [ 0 != ${rc} ] ; then
+    echo "ERROR ${rc} starting the icecast2 server."
+	exit 1
+fi
+
+
+echo ${DELIMITER}
+echo "Wait briefly for the server to start."
+sleep 3
+
+
+echo ${DELIMITER}
+echo "Verify the icecast port ${ICECAST_PORT} is listening."
+netstat -na | grep LISTEN | grep ${ICECAST_PORT} | grep -v grep
+rc=$?
+if [ 0 != ${rc} ] ; then
+    echo "ERROR ${rc} verifying the icecast port ${ICECAST_PORT} is listening."
+	exit 1
+fi
+
+
+echo ${DELIMITER}
+echo "Verify we can fetch statistics from the icecast server."
+curl http://admin:${ICECAST_ADMIN_PASSWORD}@localhost:8000/admin/stats
+rc=$?
+if [ 0 != ${rc} ] ; then
+    echo "ERROR ${rc} verifying we can fetch statistics from the icecast server."
+	exit 1
+fi
 
 
 echo ${DELIMITER}
